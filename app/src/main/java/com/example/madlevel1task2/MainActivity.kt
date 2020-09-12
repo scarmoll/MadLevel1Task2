b424f75e8d8ff0369ffc9d10ad0ab03d5baf140f
Oscar Mollé <scarmoll@gmail.com>
2020-09-12 12:24:09 +0200

    alles werkt huts

--- app/src/main/java/com/example/madlevel1task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1task2.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            checkanswers()
+        }
+    }
+
+    private fun checkanswers() {
+        var amountCorrect = 0
+
+        if (binding.input1.text.toString() == "T") {
+            amountCorrect += 1
+        }
+
+        if (binding.input2.text.toString() == "F") {
+            amountCorrect += 1
+        }
+
+        if (binding.input3.text.toString() == "F") {
+            amountCorrect += 1
+        }
+
+        if (binding.input4.text.toString() == "F") {
+            amountCorrect += 1
+        }
+
+        Toast.makeText(this, "Amount correct: $amountCorrect", Toast.LENGTH_LONG).show()
     }
 }
